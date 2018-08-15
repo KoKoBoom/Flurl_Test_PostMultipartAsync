503510b59e867d6fdd3f45b6e748fa0c7973fc84
Taki <mister_zheng@sina.com>
2018-08-15 17:38:38 +0800

    create

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,46 @@
+﻿using Flurl.Http;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var res1 = "http://localhost:64728/Default.aspx"
+                .PostMultipartAsync(m => m
+                    .AddString("key1", "value1")
+                    .AddStringParts(new { key2 = "value2" })
+                    .AddFile("name", "img.png")
+                    )
+                .ReceiveString()
+                .Result;
+
+            var res2 = "http://localhost:64728/Default.aspx"
+                .PostMultipartAsync(m => m
+                    .AddString("\"quoted-key1\"", "value1")
+                    .AddStringParts(new { key2 = "value2" })
+                    .AddFile("\"quoted-name\"", "img.png")
+                    )
+                .ReceiveString()
+                .Result;
+
+            var res3 = "http://localhost:64728/Default.aspx"
+                .PostMultipartAsync(m => m
+                    .AddString("key1", "value1")
+                    .AddStringParts(new { key2 = "value2" })
+                    .AddFile("name", File.OpenRead("img.png"), "fileName"))
+                .ReceiveString()
+                .Result;
+
+            var res4 = "http://localhost:64728/Default.aspx"
+                .PostMultipartAsync(m => m
+                    .AddString("\"key1\"", "value1")
+                    .AddStringParts(new { key2 = "value2" })
+                    .AddFile("\"quoted-name\"", File.OpenRead("img.png"), "\"quoted-fileName\""))
+                .ReceiveString()
+                .Result;
+        }
+    }
+}
---
+++ WebSite1/Default.aspx
@@ -0,0 +1,14 @@
+﻿<%@ Page Language="C#" %>
+
+<%
+    StringBuilder str = new StringBuilder();
+    for (int i = 0; i < Request.Form.Count; i++)
+    {
+        str.AppendLine("    " + Request.Form.GetKey(i) + ":" + Request.Form[i]);
+    }
+    for (int i = 0; i < HttpContext.Current.Request.Files.Count; i++)
+    {
+        str.AppendLine("FileName:" + HttpContext.Current.Request.Files[i].FileName);
+    }
+    Response.Write("Form.Count：" + Request.Form.Count + "\r\nfileList.Count:" + HttpContext.Current.Request.Files.Count + "\r\nkeyValue:\r\n" + str);
+%>
